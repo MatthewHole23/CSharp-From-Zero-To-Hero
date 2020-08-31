599b01fa0554c580e54ed027c191ce5d1d9e6e30
Matthew Hole <matthew.hole23@gmail.com>
2020-08-31 10:59:22 +0100

    Initial Commit - Creation of the 3 test suites. Began populating the CardTests Suite with tests.

---
+++ Tests/BootCamp.Chapter.Tests/DeckTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BootCamp.Chapter.Gambling;
+using Xunit;
+
+namespace BootCamp.Chapter.Tests
+{
+    public class DeckTests
+    {
+        [Fact]
+        public void Instantiating_Deck_When_CardsEmpty_Throws_ArgumentNullException()
+        {
+            IList<Card> cards = default;
+
+            var exception = Assert.Throws<ArgumentNullException>(() => new Deck(cards));
+
+            Assert.Equal($"Value cannot be null. (Parameter '{nameof(cards)}')", exception.Message);
+        }
+
+        [Fact]
+        public void Instantiating_Deck_When_CardsNotEmpty_Does_Not_Throw_Exception()
+        {
+            IList<Card> cards = new List<Card>() {new Card(Card.Suites.Clubs, Card.Ranks.Ace)};
+
+            var exception = Record.Exception(() => new Deck(cards));
+
+            Assert.Null(exception);
+        }
+
+
+    }
+}
